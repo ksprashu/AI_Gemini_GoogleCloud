388d660e15837d9e8a30023bbfd02658bae00148
Prashanth Subrahmanyam <ksprashanth@google.com>
2024-11-17 14:30:11 +0000

    added logic to fetch prompts from latest format

--- week6/helper.py
+++ week6/helper.py
@@ -19,7 +19,7 @@ def store_chat(session_id, message, response, history):
     """Stores the chat history in Firestore and updates it against a UUID."""
 
     try:
-        doc_ref = db.collection("chat_history").document(session_id)
+        doc_ref = db.collection("chat-history").document(session_id)
         # append message and response to a copy of history
         chat_messages = history.copy()
         chat_messages.append({"role": "user", "content": message})
@@ -32,27 +32,72 @@ def store_chat(session_id, message, response, history):
         logging.error(f"Error storing chat history: {e}")
 
 
+# Function to fetch the entire chat history from Firestore
+def fetch_chats():
+    """Fetch the entire chat history from Firestore."""
+    try:
+        docs = db.collection("chat-history").stream()
+        chat_history = []
+        for doc in docs:
+            # collect only the docs where the role is "user"
+            chat_messages = doc.to_dict()['chat_messages']
+            chat_history = [msg['content'] for msg in chat_messages if msg['role'] == 'user']
+
+        logging.info(f"fetched {len(chat_history)} chat history records")
+        return chat_history
+    
+    except Exception as e:
+        logging.error(f"Error fetching chat history: {e}")
+        return []
+
+
 # Function to generate a unique session ID
 def generate_session_id():
   """Generates a unique session ID using the UUID library."""
   return str(uuid.uuid4())
 
 
-# Gets the latest system prompt for a given model
-def get_system_prompt(model_name):
-    """Gets the latest system prompt for a given model."""
-    # Get the directory of helper.py
-    helper_dir = os.path.dirname(os.path.abspath(__file__))
-    # Construct the path to config.json
-    config_path = os.path.join(helper_dir, 'prompts.json')
+# Function to get latest prompts for a given use case
+def get_use_case(use_case):
+    """Gets the latest prompts for a given use case."""
+    try:
+        # Get the directory of helper.py
+        helper_dir = os.path.dirname(os.path.abspath(__file__))
+        # Construct the path to config.json
+        config_path = os.path.join(helper_dir, 'prompts.json')
 
-    with open(config_path) as f:
-        data = json.load(f)
+        with open(config_path) as f:
+            data = json.load(f)
+        
+        # find the latest version of the prompt
+        latest_prompt = None
+        for prompt_data in data[use_case]:
+            if latest_prompt is None or prompt_data['version'] > latest_prompt['version']:
+                latest_prompt = prompt_data
+        
+        return latest_prompt
+    except Exception as e:
+        logging.error(f"Error getting use case: {e}")
+        return None
+
+
+
+# Gets the latest system prompt for a given model
+def get_system_prompt(use_case):
+    """Gets the latest system prompt for a given model."""
+    prompt = get_use_case(use_case)
+    if prompt is not None:
+        return "\n".join(prompt['system_prompt'])
+    else:
+        return None
+
+
+# Gets the latest query prompt for the given use case
+def get_input_prompt(use_case):
+    """Gets the latest query prompt for the given use case."""
+    prompt = get_use_case(use_case)
+    if prompt is not None:
+        return "\n".join(prompt['input_prompt'])
+    else:
+        return None
     
-    # find the latest version of the prompt
-    latest_prompt = None
-    for prompt_data in data[model_name]:
-        if latest_prompt is None or prompt_data['version'] > latest_prompt['version']:
-            latest_prompt = prompt_data
-    
-    return "\n".join(latest_prompt['system_prompt'])
